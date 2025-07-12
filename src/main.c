2a518a0917d230ed2637f8b65aa51264555cb069
daromi94 <davromero07@gmail.com>
2025-07-11 23:04:49 -0500

    feat: :sparkles: add request line struct and alloc/free functions

--- src/main.c
+++ src/main.c
@@ -1,11 +1,58 @@
-#include <stdio.h>
-#include <sysexits.h>
+#include <stdlib.h>
+
+typedef struct request_line
+{
+    char *method;
+    char *request_target;
+    char *http_version;
+} RequestLine;
+
+RequestLine *parse_request_line(const char *raw_request_line)
+{
+    RequestLine *request_line = (RequestLine *)malloc(sizeof(RequestLine));
+    if (request_line == NULL)
+    {
+        return NULL;
+    }
+
+    request_line->method = NULL;
+    request_line->request_target = NULL;
+    request_line->http_version = NULL;
+
+    // TODO: actual parsing
+
+    return request_line;
+}
+
+void destroy_request_line(RequestLine *request_line)
+{
+    if (request_line == NULL)
+    {
+        return;
+    }
+
+    free(request_line->method);
+    free(request_line->request_target);
+    free(request_line->http_version);
+    request_line->method = NULL;
+    request_line->request_target = NULL;
+    request_line->http_version = NULL;
+
+    free(request_line);
+    request_line = NULL;
+}
 
 int main()
 {
-    const char *header = "HTTP/1.1 200 OK";
+    const char *raw_request_line = "GET /hello HTTP 1.1\r\n";
 
-    fprintf(stdout, "%s\n", header);
+    RequestLine *request_line = parse_request_line(raw_request_line);
+    if (request_line == NULL)
+    {
+        return EXIT_FAILURE;
+    }
+    destroy_request_line(request_line);
+    request_line = NULL;
 
-    return EX_OK;
+    return EXIT_SUCCESS;
 }
