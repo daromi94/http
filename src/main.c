692d397317f8d3e4e74cc0de7629ab635ffebaf0
daromi94 <davromero07@gmail.com>
2025-07-12 23:30:35 -0500

    feat: :sparkles: extract method from raw request line

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct RequestLine
 {
@@ -6,50 +9,6 @@ typedef struct RequestLine
     char *request_target;
     char *http_version;
 } RequestLine;
-
-RequestLine *parse_request_line(const char *raw_request_line)
-{
-    RequestLine *request_line = (RequestLine *)malloc(sizeof(RequestLine));
-    if (request_line == NULL)
-    {
-        return NULL;
-    }
-
-    request_line->method = NULL;
-    request_line->request_target = NULL;
-    request_line->http_version = NULL;
-
-    // format: method SP request-target SP HTTP-version CRLF
-
-    // 1. extract method
-    char *method = (char *)malloc(42 * sizeof(char));
-    if (method == NULL)
-    {
-        return NULL;
-    }
-
-    request_line->method = method;
-
-    // 2. extract request-target
-    char *request_target = (char *)malloc(42 * sizeof(char));
-    if (request_target == NULL)
-    {
-        return NULL;
-    }
-
-    request_line->request_target = request_target;
-
-    // 3. extract HTTP-version
-    char *http_version = (char *)malloc(42 * sizeof(char));
-    if (request_target == NULL)
-    {
-        return NULL;
-    }
-
-    request_line->http_version = http_version;
-
-    return request_line;
-}
 
 void destroy_request_line(RequestLine *request_line)
 {
@@ -69,6 +28,101 @@ void destroy_request_line(RequestLine *request_line)
     request_line = NULL;
 }
 
+void print_request_line(const RequestLine *request_line)
+{
+    fprintf(stdout, "%s %s %s\n", request_line->method, request_line->request_target, request_line->http_version);
+}
+
+int parse_method(const char *raw_value, const int raw_length, char **output)
+{
+    // Check and initialize output
+    if (output == NULL)
+    {
+        // TODO: error message
+        return -1;
+    }
+    *output = NULL;
+
+    // Check raw value and length
+    if (raw_value == NULL || raw_length <= 0)
+    {
+        // TODO: error message
+        return -1;
+    }
+
+    // Find first space
+    int space_index = -1;
+
+    for (int i = 0; i < raw_length; i++)
+    {
+        if (raw_value[i] == ' ')
+        {
+            space_index = i;
+            break;
+        }
+    }
+
+    if (space_index <= 0)
+    {
+        // TODO: error message
+        return -1;
+    }
+
+    const int method_length = space_index;
+
+    // Allocate output
+    *output = (char *)malloc((method_length + 1) * sizeof(char));
+    if (*output == NULL)
+    {
+        // TODO: error message
+        return -1;
+    }
+
+    // Copy content
+    memcpy(*output, raw_value, method_length * sizeof(char));
+
+    // Place null terminator
+    (*output)[method_length] = '\0';
+
+    return space_index;
+}
+
+RequestLine *parse_request_line(const char *raw_value)
+{
+    // Check raw value
+    if (raw_value == NULL)
+    {
+        // TODO: error message
+        return NULL;
+    }
+
+    // Allocate request line
+    RequestLine *request_line = (RequestLine *)malloc(sizeof(RequestLine));
+    if (request_line == NULL)
+    {
+        // TODO: error message
+        return NULL;
+    }
+
+    request_line->method = NULL;
+    request_line->request_target = NULL;
+    request_line->http_version = NULL;
+
+    // Parse and assign elements
+    const int raw_length = strlen(raw_value);
+
+    if (parse_method(raw_value, raw_length, &request_line->method) == -1)
+    {
+        destroy_request_line(request_line);
+        return NULL;
+    }
+
+    // TODO: parse request target
+    // TODO: parse http version
+
+    return request_line;
+}
+
 int main()
 {
     const char *raw_request_line = "GET /hello HTTP 1.1\r\n";
@@ -78,8 +132,8 @@ int main()
     {
         return EXIT_FAILURE;
     }
+    print_request_line(request_line);
     destroy_request_line(request_line);
-    request_line = NULL;
 
     return EXIT_SUCCESS;
 }
